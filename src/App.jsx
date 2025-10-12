c7bcf51701b019ea51780981912498ea08daf3bf
Simauniha <2005.simaunihao@email.com>
2025-10-12 18:55:34 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./Component/LandingPageComponent/Navbar";
 import About from "./Component/LandingPageComponent/about";
 import Service from "./Component/LandingPageComponent/service";
@@ -14,36 +14,44 @@ import Login from "./Component/LandingPageComponent/Login";
 import Schedule from "./Component/LandingPageComponent/Schedule";
 import Contact from "./Component/LandingPageComponent/Contact"
 import Home1 from "./Component/LandingPageComponent/Home1"
-import "./App.css";   
+import NotFound from "./Component/LandingPageComponent/NotFound";
+
 function App() {
+  const location = useLocation()
+  const [userData, setUserData] = useState(null)
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"))
+    setUserData(user)
+  }, [location])
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/footer" element={<Footer />} />
-          <Route path="/membership" element={<Register />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/Schedule" element={<Schedule />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Home1" element={<Home1 />} />
-          <Route path="/login" element={<Login />} />
-          {/* {User Route} */}
-          {userData?.userType == "user" && <>
-            <Route path="/userDashboard" element={<UserDashboard />} />
-          </>}
 
-          {/* {Admin Route} */}
-          {userData?.userType == "admin" && <>
-            <Route path="/adminDashboard" element={<AdminDashboard />} />
-          </>}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/membership" element={<Register />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/Schedule" element={<Schedule />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Home1" element={<Home1 />} />
+        <Route path="/login" element={<Login />} />
 
-          <Route path="*" element={<NotFound />} />
+        {/* {User Route} */}
+        {userData?.userType == "user" && <>
+          <Route path="/userDashboard" element={<UserDashboard />} />
+        </>}
 
-        </Routes>
-      </Router>
+        {/* {Admin Route} */}
+        {userData?.userType == "admin" && <>
+          <Route path="/adminDashboard" element={<AdminDashboard />} />
+        </>}
+
+        <Route path="*" element={<NotFound/>} />
+
+      </Routes>
     </>
   );
 }
