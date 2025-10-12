69ebea94347f2dc882b7946cd09c3ddbee6527fb
Simauniha <2005.simaunihao@email.com>
2025-10-12 18:35:32 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import Blog from "./Component/LandingPageComponent/Blog";
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./Component/LandingPageComponent/Register";
-import "./App.css";   
+import "./App.css";
 import Login from "./Component/LandingPageComponent/Login";
 import Schedule from "./Component/LandingPageComponent/Schedule";
 import Contact from "./Component/LandingPageComponent/Contact"
 import Home1 from "./Component/LandingPageComponent/Home1"
-import "./App.css";   //dfyfdg
+import "./App.css";   
 function App() {
   return (
     <>
@@ -26,10 +26,22 @@ function App() {
           <Route path="/footer" element={<Footer />} />
           <Route path="/membership" element={<Register />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/Schedule" element={<Schedule/>} />
-          <Route path="/Contact" element={<Contact/>} />
-           <Route path="/Home1" element={<Home1/>} />
+          <Route path="/Schedule" element={<Schedule />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Home1" element={<Home1 />} />
           <Route path="/login" element={<Login />} />
+          {/* {User Route} */}
+          {userData?.userType == "user" && <>
+            <Route path="/userDashboard" element={<UserDashboard />} />
+          </>}
+
+          {/* {Admin Route} */}
+          {userData?.userType == "admin" && <>
+            <Route path="/adminDashboard" element={<AdminDashboard />} />
+          </>}
+
+          <Route path="*" element={<NotFound />} />
+
         </Routes>
       </Router>
     </>
