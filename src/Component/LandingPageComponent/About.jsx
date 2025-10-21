cbfeb4788ab8dfcea14428b0e438b94c21a72222
Simauniha <2005.simaunihao@email.com>
2025-10-21 17:47:54 +0530

    feedback

--- src/Component/LandingPageComponent/About.jsx
+++ src/Component/LandingPageComponent/About.jsx
@@ -4,6 +4,7 @@ import Trainer from "./Trainer";
 import HoverImage from "./HoverImage";
 import GalleryImage from "./GalleryImage";
 import AboutGym from "./AboutGym";
+import Feedback from "./Feedback";
 
 
 const About = () => {
@@ -18,40 +19,11 @@ const About = () => {
       </div>
 
 
-      <AboutGym/>
+      <AboutGym />
 
-      <GalleryImage/>
+      <GalleryImage />
 
-      <div className="row">
-        <div className="col-sm-2"></div>
-        <div className="col-sm-4 about-img6">
-          <HoverImage src="/images/about2.png" alt="" />
-        </div>
-        <div className="col-sm-4">
-           
-          <h5 className="hero-section">CLIENT FEEDBACK</h5>
-          <h1 className="hero-heading">
-            WHAT OUR CLIENT THINK
-            <br /> ABOUT YOUR GYM
-          </h1>
-         
-          <p className="para3">
-            Brook presents your services with flexible, convenient and cdpose
-            <br /> layouts.You can select your favorite layouts & elements for
-            cular ts with <br />
-            unlimited customization possibilities. Pixel-perfect replication of
-            the <br />
-            designers is intended.
-          </p>
-          <h5 className="hero-caption">JOHN SMITH</h5>
-          <h6 className="hero-caption2">Gym Trainer</h6>
-            
-        </div>
-        <div className="col-sm-2">
-     
-        </div>
-
-      </div>
+      <Feedback />
 
       <Trainer />
 
